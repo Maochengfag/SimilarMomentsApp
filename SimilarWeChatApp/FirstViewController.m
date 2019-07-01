34ca068e91e674fd8b6642524a68aedad64bb13c
Oliver_Mao <maochengfang@126.com>
2019-07-01 09:02:24 +0800

    Initial Commit

---
+++ SimilarWeChatApp/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  SimilarWeChatApp
+//
+//  Created by Mac on 2019/7/1.
+//  Copyright © 2019年 Mac. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ SimilarWeChatApp/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  SimilarWeChatApp
+//
+//  Created by Mac on 2019/7/1.
+//  Copyright © 2019年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
